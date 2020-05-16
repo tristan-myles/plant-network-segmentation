3b64d673ed2ac76574065fc3bba93960cbd46eb5
Tristan Naidoo <mylesnaidoo95@gmail.com>
2020-05-16 23:25:44 +0200

    Adds argparser and code to create sequence objects
    - adds code to create MaskSequence and LeafSequence objects using a json
      file
    - the json file must contain the headings leaves and masks, the
      sub-headings under these must be the same as the applicable
      LeafSequence and MaskSequence constructor arguments

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,9 @@
-import logging
+import argparse
+import json
 import logging.config
-
 from os import path
+
+from src.data.data_model import *
 
 abs_path = path.dirname(path.abspath(__file__))
 
@@ -10,5 +12,41 @@ logging.config.fileConfig(fname=abs_path + "/logging_configuration.ini",
                           disable_existing_loggers=False)
 LOGGER = logging.getLogger(__name__)
 
+
+def create_sequence_objects(json_path: str):
+    lseqs = []
+    mseqs = []
+
+    with open(json_path, "r") as json_file:
+        sequence_input = json.load(json_file)
+
+    leaf_keys = list(sequence_input["leaves"].keys())
+    if sequence_input["leaves"] is not None:
+        for vals in zip(*list(sequence_input["leaves"].values())):
+            kwargs = {key: val for key, val in zip(leaf_keys, vals)}
+            lseqs.append(LeafSequence(**kwargs))
+
+    mask_keys = list(sequence_input["masks"].keys())
+    if sequence_input["masks"] is not None:
+        for vals in zip(*list(sequence_input["masks"].values())):
+            kwargs = {key: val for key, val in zip(mask_keys, vals)}
+            mseqs.append(MaskSequence(**kwargs))
+
+    return lseqs, mseqs
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("Perform operations using the "
+                                     "plant-image-segmentation code base")
+    parser.add_argument("json_path", type=str,
+                        help="path to a JSON file with the required "
+                             "parameters to create LeafSequence and "
+                             "MaskSequence objects")
+
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
-    pass
+    ARGS = parse_arguments()
+    LSEQS, MSEQS = create_sequence_objects(ARGS.json_path)
