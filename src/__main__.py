bbee12e9b0de2a24ecd8d0c6ea43811dab083ef6
Tristan Naidoo <mylesnaidoo95@gmail.com>
2020-05-18 21:23:45 +0200

    Adds code to extract tiles
    - adds load_image_objects function to create image objects under the sequence
      objects
    - adds extract_tiles function
    - adds extract_tiles subparser
    - 3 ways to provide the output path

--- src/__main__.py
+++ src/__main__.py
@@ -12,6 +12,9 @@ logging.config.fileConfig(fname=abs_path + "/logging_configuration.ini",
                           disable_existing_loggers=False)
 LOGGER = logging.getLogger(__name__)
 
+pil_logger = logging.getLogger('PIL')
+pil_logger.setLevel(logging.WARNING)
+
 
 def create_sequence_objects(sequence_input):
     lseqs = []
@@ -34,6 +37,15 @@ def create_sequence_objects(sequence_input):
     return lseqs, mseqs
 
 
+def load_image_objects(seq_objects, load_images=False):
+    LOGGER.info(f"Creating image objects for "
+                f"{seq_objects[0].__class__.__name__}")
+
+    for seq in seq_objects:
+        LOGGER.info(f"Creating {seq.num_files} objects")
+        seq.load_extracted_images(load_image=load_images)
+
+
 def extract_leaf_images(lseq_list, output_path_list):
     LOGGER.info("Extracting differenced leaf images")
     for lseq, output_path in zip(lseq_list, output_path_list):
@@ -48,6 +60,24 @@ def extract_multipage_mask_images(mseq_list, output_path_list):
         LOGGER.info(f"Extracting images from: {mseq.mpf_path} and saving "
                     f"to {output_path}")
         mseq.extract_mask_from_multipage(output_path)
+
+
+def extract_tiles(seq_objects, length_x, stride_x, length_y,
+                  stride_y, output_path_list=None):
+    LOGGER.info(f"Extracting tiles from {seq_objects[0].__class__.__name__} "
+                f"with the following configuration:"
+                f" length (x, y): ({length_x}, {length_y}) |"
+                f" stride (x, y): ({stride_x}, {stride_y})")
+
+    if output_path_list is None:
+        for seq in seq_objects:
+            seq.tile_sequence(length_x=length_x, stride_x=stride_x,
+                              length_y=length_y, stride_y=stride_y)
+    else:
+        for seq, output_path in zip(seq_objects, output_path_list):
+            seq.tile_sequence(length_x=length_x, stride_x=stride_x,
+                              length_y=length_y, stride_y=stride_y,
+                              output_path=output_path)
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -77,6 +107,31 @@ def parse_arguments() -> argparse.Namespace:
         help="path to json file containing output paths, if the path is the"
              " same as the input path enter  \"same\" ")
 
+    parser_extract_tiles = subparsers.add_parser("extract_tiles",
+                                                  help="extraction help")
+    parser_extract_tiles.set_defaults(which="extract_tiles")
+
+    parser_extract_tiles.add_argument("-sx", "--stride_x", metavar="\b",
+                                      type=int, help="x stride size")
+    parser_extract_tiles.add_argument("-sy", "--stride_y", metavar="\b",
+                                      type=int, help="y stride size")
+    parser_extract_tiles.add_argument("-lx", "--length_x", metavar="\b",
+                                      type=int, help="tile x length")
+    parser_extract_tiles.add_argument("-ly", "--length_y", metavar="\b",
+                                      type=int, help="tile y length")
+
+    parser_extract_tiles.add_argument(
+        "--leaf_output_path", "-lo",  metavar="\b",
+        help="path to json file containing output paths, if you want to use "
+             "the default path enter  \"default\", if the path is the same "
+             "as the input json enter  \"same\" ")
+
+    parser_extract_tiles.add_argument(
+        "--mask_output_path", "-mo", metavar="\b",
+        help="path to json file containing output paths, if you want to use "
+             "the default path enter  \"default\", if the path is the same "
+             "as the input json enter  \"same\"")
+
     args = parser.parse_args()
     return args
 
@@ -84,6 +139,7 @@ def parse_arguments() -> argparse.Namespace:
 if __name__ == "__main__":
     ARGS = parse_arguments()
 
+    LOGGER.debug(ARGS.which)
     with open(ARGS.json_path, "r") as JSON_FILE:
         INPUT_JSON_DICT = json.load(JSON_FILE)
 
@@ -107,3 +163,32 @@ if __name__ == "__main__":
                 MASK_OUTPUT_LIST = [ARGS.mask_output_path]
 
             extract_multipage_mask_images(MSEQS, MASK_OUTPUT_LIST)
+
+    if ARGS.which == "extract_tiles":
+        if ARGS.leaf_output_path is not None:
+            load_image_objects(LSEQS)
+
+            if ARGS.leaf_output_path == "default":
+                LEAF_OUTPUT_LIST = None
+            elif ARGS.leaf_output_path == "same":
+                LEAF_OUTPUT_LIST =\
+                    INPUT_JSON_DICT["leaves"]["output"]["output_path"]
+            else:
+                LEAF_OUTPUT_LIST = str.split(ARGS.leaf_output_path, " ")
+
+            extract_tiles(LSEQS, ARGS.length_x, ARGS.stride_x,
+                          ARGS.length_y, ARGS.stride_y, LEAF_OUTPUT_LIST)
+
+        if ARGS.mask_output_path is not None:
+            load_image_objects(MSEQS)
+
+            if ARGS.mask_output_path == "default":
+                MASK_OUTPUT_LIST = None
+            elif ARGS.leaf_output_path == "same":
+                MASK_OUTPUT_LIST = \
+                    INPUT_JSON_DICT["masks"]["output"]["output_path"]
+            else:
+                MASK_OUTPUT_LIST = str.split(ARGS.mask_output_path, " ")
+
+            extract_tiles(MSEQS, ARGS.length_x, ARGS.stride_x,
+                          ARGS.length_y, ARGS.stride_y, MASK_OUTPUT_LIST)
