98d7c01e29d3defffbad0224411e43bb2418e385
Tristan Naidoo <mylesnaidoo95@gmail.com>
2020-05-18 14:13:14 +0200

    Adds code to extract (full) images
    - adds extract_images subparser
    - code applies to either a list of image sequences or a single image
      sequence
    - if extraction pertains to more than one sequence the output paths need
      to be provided in the input json and "same" needs to be passed as
      the output path
    - adds the functions: extract_leaf_images, extract_multipage_mask_images

--- src/__main__.py
+++ src/__main__.py
@@ -20,28 +20,65 @@ def create_sequence_objects(json_path: str):
     with open(json_path, "r") as json_file:
         sequence_input = json.load(json_file)
 
-    leaf_keys = list(sequence_input["leaves"].keys())
-    if sequence_input["leaves"] is not None:
-        for vals in zip(*list(sequence_input["leaves"].values())):
+    leaf_dict = sequence_input["leaves"]["input"]
+    if leaf_dict is not None:
+        leaf_keys = list(leaf_dict.keys())
+        for vals in zip(*list(leaf_dict.values())):
             kwargs = {key: val for key, val in zip(leaf_keys, vals)}
             lseqs.append(LeafSequence(**kwargs))
 
-    mask_keys = list(sequence_input["masks"].keys())
-    if sequence_input["masks"] is not None:
-        for vals in zip(*list(sequence_input["masks"].values())):
+    mask_dict = sequence_input["masks"]["input"]
+    if mask_dict is not None:
+        mask_keys = list(mask_dict.keys())
+        for vals in zip(*list(mask_dict.values())):
             kwargs = {key: val for key, val in zip(mask_keys, vals)}
             mseqs.append(MaskSequence(**kwargs))
 
     return lseqs, mseqs
 
 
+def extract_leaf_images(lseq_list, output_path_list):
+    LOGGER.info("Extracting differenced leaf images")
+    for lseq, output_path in zip(lseq_list, output_path_list):
+        LOGGER.info(f"Differencing images in {lseq.folder_path} and saving "
+                    f"to {output_path}")
+        lseq.extract_changed_leaves(output_path)
+
+
+def extract_multipage_mask_images(mseq_list, output_path_list):
+    LOGGER.info("Extracting mask images from multipage file")
+    for mseq, output_path in zip(mseq_list, output_path_list):
+        LOGGER.info(f"Extracting images from: {mseq.mpf_path} and saving "
+                    f"to {output_path}")
+        mseq.extract_mask_from_multipage(output_path)
+
+
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser("Perform operations using the "
                                      "plant-image-segmentation code base")
+
     parser.add_argument("json_path", type=str,
                         help="path to a JSON file with the required "
                              "parameters to create LeafSequence and "
                              "MaskSequence objects")
+
+    subparsers = parser.add_subparsers(title="actions",
+                                       description='possible actions using '
+                                                   'this module')
+
+    parser_extract_images = subparsers.add_parser("extract_images",
+                                              help="extraction help")
+    parser_extract_images.set_defaults(which='extract_images')
+
+    parser_extract_images.add_argument(
+        "--leaf_output_path", "-lo",  metavar="\b",
+        help="path to json file containing output paths, if the path is the"
+             " same as the input path enter  \"same\" ")
+
+    parser_extract_images.add_argument(
+        "--mask_output_path", "-mo", metavar="\b",
+        help="path to json file containing output paths, if the path is the"
+             " same as the input path enter  \"same\" ")
 
     args = parser.parse_args()
     return args
@@ -50,3 +87,26 @@ def parse_arguments() -> argparse.Namespace:
 if __name__ == "__main__":
     ARGS = parse_arguments()
     LSEQS, MSEQS = create_sequence_objects(ARGS.json_path)
+
+    if ARGS.which == "extract_images":
+        if ARGS.mask_output_path == "same":
+            with open(ARGS.json_path, "r") as JSON_FILE:
+                INPUT_JSON_DICT = json.load(JSON_FILE)
+
+        if ARGS.leaf_output_path is not None:
+            if ARGS.leaf_output_path == "same":
+                LEAF_OUTPUT_LIST =\
+                    INPUT_JSON_DICT["leaves"]["output"]["output_path"]
+            else:
+                LEAF_OUTPUT_LIST = [ARGS.leaf_output_path]
+
+            extract_leaf_images(LSEQS, LEAF_OUTPUT_LIST)
+
+        if ARGS.mask_output_path is not None:
+            if ARGS.mask_output_path == "same":
+                MASK_OUTPUT_LIST = \
+                    INPUT_JSON_DICT["masks"]["output"]["output_path"]
+            else:
+                MASK_OUTPUT_LIST = [ARGS.mask_output_path]
+
+            extract_multipage_mask_images(MSEQS, MASK_OUTPUT_LIST)
