a52b843bde41832c4d8490cf758bc7c97b70d7c3
tristan-myles <mylesnaidoo95@gmail.com>
2020-10-05 23:01:06 +0200

    Refactors fai __main__;moves code to fai utilities
    - moves procedural and argparse code from src.pipelines.fast_ai_1.__main__ to fai utilities
      script

--- src/pipelines/fast_ai_1/__main__.py
+++ src/pipelines/fast_ai_1/__main__.py
@@ -1,13 +1,8 @@
-import argparse
 import logging.config
 from os import path
 
-from fastai.vision import *
-
-from src.data.data_model import *
-from src.pipelines.fast_ai_1.helpers.utilities import (combine_and_add_valid,
-                                                       format_databunch_df)
-from src.pipelines.fast_ai_1.model.fastai_models import FastaiUnetLearner
+from src.helpers.utilities import create_sequence_objects, load_image_objects
+from src.pipelines.fast_ai_1.helpers.utilities import *
 
 abs_path = path.dirname(path.abspath(__file__))
 
@@ -23,132 +18,7 @@ mpl_logger = logging.getLogger('matplotlib')
 mpl_logger.setLevel(logging.WARNING)
 
 
-def create_sequence_objects(sequence_input):
-    lseqs = []
-    mseqs = []
-
-    leaf_dict = sequence_input["leaves"]["input"]
-    if leaf_dict is not None:
-        leaf_keys = list(leaf_dict.keys())
-        for vals in zip(*list(leaf_dict.values())):
-            kwargs = {key: val for key, val in zip(leaf_keys, vals)}
-            lseqs.append(LeafSequence(**kwargs))
-
-    mask_dict = sequence_input["masks"]["input"]
-    if mask_dict is not None:
-        mask_keys = list(mask_dict.keys())
-        for vals in zip(*list(mask_dict.values())):
-            kwargs = {key: val for key, val in zip(mask_keys, vals)}
-            mseqs.append(MaskSequence(**kwargs))
-
-    return lseqs, mseqs
-
-
-def load_image_objects(seq_objects, load_images=False):
-    LOGGER.info(f"Creating image objects for "
-                f"{seq_objects[0].__class__.__name__}")
-
-    for seq in seq_objects:
-        LOGGER.info(f"Creating {seq.num_files} objects")
-        seq.load_extracted_images(load_image=load_images)
-
-
-def train_fastai_unet(train_df_paths, save_path, bs, epochs, lr,
-                      val_df_paths=None, unfreeze_type=None,
-                      model_weights_path=None, **kwargs):
-    training_dfs = [pd.read_csv(path, index_col=0) for path in train_df_paths]
-
-    if val_df_paths:
-        validation_dfs = [pd.read_csv(path, index_col=0) for path in
-                          val_df_paths]
-        combined_df = combine_and_add_valid(training_dfs, validation_dfs)
-    else:
-        combined_df = pd.concat(training_dfs)
-
-    combined_df, folder_name_path = format_databunch_df(
-        combined_df, "folder_path", "leaf_name", create_copy=True)
-
-    fai = FastaiUnetLearner()
-
-    if val_df_paths:
-        fai.prep_fastai_data(combined_df, folder_name_path, bs, plot=False,
-                             mask_col_name="mask_path",
-                             **kwargs)
-    else:
-        fai.prep_fastai_data(combined_df, folder_name_path, bs, plot=False,
-                             mask_col_name="mask_path",
-                             split_func=ItemList.split_by_rand_pct,
-                             **kwargs)
-
-    fai.create_learner()
-
-    if model_weights_path:
-        fai.load_weights(model_weights_path)
-
-    fai.train(epochs=epochs, save_path=save_path, lr=lr,
-              unfreeze_type=unfreeze_type)
-
-
-def predict_fastai_unet(lseqs, length_x, length_y, model_pkl_path):
-    fai_unet_learner = FastaiUnetLearner(model_pkl_path=model_pkl_path)
-
-    for lseq in lseqs:
-        lseq.predict_leaf_sequence(fai_unet_learner, length_x, length_y)
-
-
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser("Perform operations using the "
-                                     "plant-image-segmentation code base")
-
-    parser.add_argument("json_path", type=str,
-                        help="path to a JSON file with the required "
-                             "parameters to create LeafSequence and "
-                             "MaskSequence objects")
-
-    subparsers = parser.add_subparsers(title="actions",
-                                       description='possible actions using '
-                                                   'this module')
-
-    parser_train_fastai = subparsers.add_parser(
-        "train_fastai", help="train a fastai unet model")
-    parser_train_fastai.set_defaults(which="train_fastai")
-    parser_train_fastai.add_argument("--batch_size", "-bs", type=int,
-                                     metavar="\b", help="batch size")
-    parser_train_fastai.add_argument("--save_path", "-sp", type=str,
-                                     metavar="\b",
-                                     help="path to save model weights and "
-                                          "model pickle")
-    parser_train_fastai.add_argument("--epochs", "-e", type=int,
-                                     metavar="\b", help="number of epochs")
-    parser_train_fastai.add_argument("--learning_rate", "-lr", type=float,
-                                     metavar="\b", help="learning rate")
-    parser_train_fastai.add_argument(
-        "--unfreeze_type", "-ut", metavar="\b",
-        help="whether the model needs to be unfrozen before training, "
-             "the options that follow refer to the layer you want to "
-             "unfreeze: 'all', 'last', or the number you want to freeze up "
-             "to")
-    parser_train_fastai.add_argument(
-        "--model_weights_path", "-mwp",
-        help="path to fastai model weights (complete path excluding .pth) to "
-             "retrain, if the paths are in the input json enter \"same\"")
-
-    parser_predict_fastai = subparsers.add_parser(
-        "predict_fastai", help="train a fastai unet model")
-    parser_predict_fastai.set_defaults(which="predict_fastai")
-    parser_predict_fastai.add_argument("length_x", type=int,
-                                       help="tile x length")
-    parser_predict_fastai.add_argument("length_y", type=int,
-                                       help="tile y length")
-    parser_predict_fastai.add_argument(
-        "model_path", help="path to fastai pkl (complete path), if the paths "
-                           "are in the input json enter \"same\"")
-
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == "__main__":
+def main():
     ARGS = parse_arguments()
 
     LOGGER.debug(ARGS.which)
@@ -195,3 +65,7 @@ if __name__ == "__main__":
 
         predict_fastai_unet(LSEQS, ARGS.length_x, ARGS.length_y,
                             MODEL_PATH)
+
+
+if __name__ == "__main__":
+    main()
--- src/pipelines/fast_ai_1/helpers/utilities.py
+++ src/pipelines/fast_ai_1/helpers/utilities.py
@@ -1,7 +1,9 @@
-import logging
-from copy import deepcopy
+import argparse
 
-import pandas as pd
+from fastai.vision import *
+
+from src.data.data_model import *
+from src.pipelines.fast_ai_1.model.fastai_models import FastaiUnetLearner
 
 LOGGER = logging.getLogger(__name__)
 
@@ -46,3 +48,103 @@ def format_databunch_df(df_to_update, folder_col_name,
     updated_df.drop(folder_col_name, axis=1, inplace=True)
 
     return updated_df, common_folder_path
+
+
+# *================================== main ===================================*
+# *---------------------------------- train ----------------------------------*
+def train_fastai_unet(train_df_paths, save_path, bs, epochs, lr,
+                      val_df_paths=None, unfreeze_type=None,
+                      model_weights_path=None, **kwargs):
+    training_dfs = [pd.read_csv(path, index_col=0) for path in train_df_paths]
+
+    if val_df_paths:
+        validation_dfs = [pd.read_csv(path, index_col=0) for path in
+                          val_df_paths]
+        combined_df = combine_and_add_valid(training_dfs, validation_dfs)
+    else:
+        combined_df = pd.concat(training_dfs)
+
+    combined_df, folder_name_path = format_databunch_df(
+        combined_df, "folder_path", "leaf_name", create_copy=True)
+
+    fai = FastaiUnetLearner()
+
+    if val_df_paths:
+        fai.prep_fastai_data(combined_df, folder_name_path, bs, plot=False,
+                             mask_col_name="mask_path",
+                             **kwargs)
+    else:
+        fai.prep_fastai_data(combined_df, folder_name_path, bs, plot=False,
+                             mask_col_name="mask_path",
+                             split_func=ItemList.split_by_rand_pct,
+                             **kwargs)
+
+    fai.create_learner()
+
+    if model_weights_path:
+        fai.load_weights(model_weights_path)
+
+    fai.train(epochs=epochs, save_path=save_path, lr=lr,
+              unfreeze_type=unfreeze_type)
+
+
+# *--------------------------------- predict ---------------------------------*
+def predict_fastai_unet(lseqs, length_x, length_y, model_pkl_path):
+    fai_unet_learner = FastaiUnetLearner(model_pkl_path=model_pkl_path)
+
+    for lseq in lseqs:
+        lseq.predict_leaf_sequence(fai_unet_learner, length_x, length_y)
+
+
+# *-------------------------------- argparse ---------------------------------*
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("Perform operations using the "
+                                     "plant-image-segmentation code base")
+
+    parser.add_argument("json_path", type=str,
+                        help="path to a JSON file with the required "
+                             "parameters to create LeafSequence and "
+                             "MaskSequence objects")
+
+    subparsers = parser.add_subparsers(title="actions",
+                                       description='possible actions using '
+                                                   'this module')
+
+    parser_train_fastai = subparsers.add_parser(
+        "train_fastai", help="train a fastai unet model")
+    parser_train_fastai.set_defaults(which="train_fastai")
+    parser_train_fastai.add_argument("--batch_size", "-bs", type=int,
+                                     metavar="\b", help="batch size")
+    parser_train_fastai.add_argument("--save_path", "-sp", type=str,
+                                     metavar="\b",
+                                     help="path to save model weights and "
+                                          "model pickle")
+    parser_train_fastai.add_argument("--epochs", "-e", type=int,
+                                     metavar="\b", help="number of epochs")
+    parser_train_fastai.add_argument("--learning_rate", "-lr", type=float,
+                                     metavar="\b", help="learning rate")
+    parser_train_fastai.add_argument(
+        "--unfreeze_type", "-ut", metavar="\b",
+        help="whether the model needs to be unfrozen before training, "
+             "the options that follow refer to the layer you want to "
+             "unfreeze: 'all', 'last', or the number you want to freeze up "
+             "to")
+    parser_train_fastai.add_argument(
+        "--model_weights_path", "-mwp",
+        help="path to fastai model weights (complete path excluding .pth) to "
+             "retrain, if the paths are in the input json enter \"same\"")
+
+    parser_predict_fastai = subparsers.add_parser(
+        "predict_fastai", help="train a fastai unet model")
+    parser_predict_fastai.set_defaults(which="predict_fastai")
+    parser_predict_fastai.add_argument("length_x", type=int,
+                                       help="tile x length")
+    parser_predict_fastai.add_argument("length_y", type=int,
+                                       help="tile y length")
+    parser_predict_fastai.add_argument(
+        "model_path", help="path to fastai pkl (complete path), if the paths "
+                           "are in the input json enter \"same\"")
+
+    args = parser.parse_args()
+    return args
+# *===========================================================================*
