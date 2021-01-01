210aaec74f67d4faf9d341bedac0dfdb71c7ca3b
tristan-myles <39033821+tristan-myles@users.noreply.github.com>
2021-01-01 19:41:29 +0200

    Adds augment data action to plant __main__ (#33)
    * Adds create_subfolder method
    
    - reduces repeated code, and centralises the creation of subfolder names,
      which are currently: leaves and masks
    
    * Adds augmentation transformation functions
    
    - adds flipping (horizontal or vertical), translation, rotation, cropping
      shearing, and zooming in/out
    
    * Adds helper functions & function for __main__
    
    - adds the following helper functions: stack_images, save_image, augment_image,
      and augmentation_algorithm
    - adds augment_dataset runner function to use in package __main__
    
    * Adds augment_dataset to input prompt & subparser
    
    - updates requirement function to add augment_dataset requirements
    
    * Adds augment_dataset action to __main__

--- src/helpers/dataset.py
+++ src/helpers/dataset.py
@@ -3,9 +3,14 @@ import shutil
 from sklearn.model_selection import train_test_split
 
 from src.data.data_model import *
+from src.helpers.utilities import create_subfolders
 from typing import Union
+import imgaug.augmenters as iaa
 
 from pathlib import Path
+
+import random
+random.seed(3141)
 
 
 # *============================= create dataset ==============================*
@@ -27,15 +32,9 @@ def create_dataset_structure(base_dir: Union[Path, str]) -> None:
 
     for path in path_list:
         for folder in ["embolism", "no-embolism"]:
-            path.joinpath(folder, "leaves").mkdir(parents=True,
-                                                  exist_ok=True)
-            path.joinpath(folder, "masks").mkdir(parents=True,
-                                                 exist_ok=True)
+            create_subfolders(path, folder)
 
-    base_dir.joinpath("not_used", "masks").mkdir(parents=True,
-                                                 exist_ok=True)
-    base_dir.joinpath("not_used", "leaves").mkdir(parents=True,
-                                                  exist_ok=True)
+    create_subfolders(base_dir, "not_used")
 
 
 def move_data(lseqs, mseqs, dest_root_path,
@@ -259,7 +258,7 @@ def split_dataset(dataset_root_path, embolism_objects, non_embolism_objects,
         f"({round((test_size/total_size) * 100)}%)")
 
 
-# *============================ package __main__ =============================*
+# *---------------------------- package __main__ -----------------------------*
 def extract_dataset(lseqs: [LeafSequence], mseqs: [MaskSequence],
                     dataset_path: Union[Path, str],
                     downsample_split: float, test_split: float,
@@ -297,4 +296,330 @@ def extract_dataset(lseqs: [LeafSequence], mseqs: [MaskSequence],
                                                 downsample_split)
 
     split_dataset(dataset_path, emb_list, non_emb_list, test_split, val_split)
-# *===========================================================================*
+
+
+# *============================= augment dataset =============================*
+# *----------------------------- transformations -----------------------------*
+def flip_flop(dual_channel: np.array, orientation: str,
+              seed: int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param orientation:
+    :param seed:
+    :return:
+    """
+    if orientation == "horizontal":
+        flip_hr = iaa.Fliplr(seed=seed)
+        flipped_images = flip_hr.augment_image(dual_channel)
+    elif orientation == "vertical":
+        flip_vr = iaa.Flipud(seed=seed)
+        flipped_images = flip_vr.augment_image(dual_channel)
+    else:
+        raise ValueError("please provide either 'horizontal' or 'vertical as "
+                         "the orientation'")
+
+    return flipped_images
+
+
+def translate_img(dual_channel: np.array, x: float, y: float,
+                  seed:int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param x:
+    :param y:
+    :param seed:
+    :return:
+    """
+    rotate = iaa.Affine(translate_percent=(x, y), seed=seed)
+    return rotate.augment_image(dual_channel)
+
+
+def rotate_img(dual_channel: np.array, l: float, r: float,
+               seed: int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param l:
+    :param r:
+    :param seed:
+    :return:
+    """
+    rotate = iaa.Affine(rotate=(l, r), seed=seed)
+    return rotate.augment_image(dual_channel)
+
+
+def shear_img(dual_channel: np.array, l: float, r: float,
+              seed: int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param l:
+    :param r:
+    :param seed:
+    :return:
+    """
+    # Shear in degrees
+    shear = iaa.Affine(shear=(l, r), seed=seed)
+    return shear.augment_image(dual_channel)
+
+
+def crop_img(dual_channel: np.array, v: float, h: float,
+             seed: int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param v:
+    :param h:
+    :param seed:
+    :return:
+    """
+    crop = iaa.Crop(percent=(v, h), seed=seed)
+    return crop.augment_image(dual_channel)
+
+
+def zoom_in_out(dual_channel: np.array, x: float, y: float,
+                seed: int = 3141) -> np.array:
+    """
+
+    :param dual_channel:
+    :param x:
+    :param y:
+    :param seed:
+    :return:
+    """
+    scale_im = iaa.Affine(scale={"x": x, "y": y}, seed=seed)
+    return scale_im.augment_image(dual_channel)
+
+
+# *--------------------------------- helpers ---------------------------------*
+def stack_images(leaf_image: np.array, mask_image: np.array) -> np.array:
+    """
+
+    :param leaf_image:
+    :param mask_image:
+    :return:
+    """
+    # Change back to 0 if not float
+    # Stack leaf and mask together so that they can be augmented uniformly
+    leaf_image = leaf_image[:, :, np.newaxis]
+    mask_image = mask_image[:, :, np.newaxis]
+
+    # leaf first
+    dual_channel = np.concatenate((leaf_image, mask_image), axis=2)
+
+    return dual_channel
+
+
+def save_image(image: np.array, leaf_path: str, mask_path: str,
+               aug_type: str) -> None:
+    """
+
+    :param image:
+    :param leaf_path:
+    :param mask_path:
+    :param aug_type:
+    :return:
+    """
+    old_paths = [leaf_path, mask_path]
+    # ["leaf", "mask"]
+    new_paths = ["", ""]
+
+    for i, path in enumerate(old_paths):
+        # requires default dataset folder structure
+        path_list = list(Path(path).parts)
+        path_list[-3] = "augmented"
+
+        # requires default naming
+        filename, ext = path_list[-1].rsplit(".", 1)
+
+        # add the description to the file name, after the image, and tile
+        # number to keep images tiles grouped
+        filename = ".".join(["_".join([filename, aug_type]), ext])
+
+        path_list[-1] = filename
+
+        new_paths[i] = Path(*path_list)
+
+    # leaf is first in stacked array
+    cv2.imwrite(str(new_paths[0]), image[:, :, 0])
+    cv2.imwrite(str(new_paths[1]), image[:, :, 1])
+
+
+def augment_image(stacked_image: np.array, df: pd.DataFrame, aug_type: str,
+                  index: int, counts: [int, int], leaf_path: str,
+                  mask_path: str, func, **kwargs) -> [int, int]:
+    """
+
+    :param stacked_image:
+    :param df:
+    :param aug_type:
+    :param index:
+    :param counts:
+    :param leaf_path:
+    :param mask_path:
+    :param func:
+    :param kwargs:
+    :return:
+    """
+    image = func(stacked_image, **kwargs)
+
+    # only save an image if it has an embolism
+    # binary segmentation problem so we know that if there are two pixel
+    # intensities there are embolisms
+
+    if len(np.unique(image[:, :, 1])) > 1:
+        save_image(image, leaf_path, mask_path, aug_type)
+
+        df[aug_type][index] = ', '.join(
+            [f'{k}: {v}' for k, v in kwargs.items()])
+
+        counts[0] += 1
+    else:
+        counts[1] += 1
+
+    return counts
+
+
+def augmentation_algorithm(dual_channel: np.array, aug_df: pd.DataFrame,
+                           i: int, leaf_path: str, mask_path: str,
+                           counts: [int, int]) -> (pd.DataFrame, [int, int]):
+    """
+
+    :param dual_channel:
+    :param aug_df:
+    :param i:
+    :param leaf_path:
+    :param mask_path:
+    :param counts:
+    :return:
+    """
+    # P(flip) = 0.35
+    if random.random() < 0.35:
+        # P(H | flip) = 0.5 | P(V | flip) = 0.5
+        if random.random() < 0.5:
+            orientation = "horizontal"
+        else:
+            orientation = "vertical"
+
+        counts = augment_image(dual_channel, aug_df, "flip", i, counts,
+                               leaf_path, mask_path, flip_flop,
+                               orientation=orientation)
+
+    # P(translate) = 0.35
+    if random.random() < 0.35:
+        # zoom in and out between -25% and 25%
+        x_per = round(random.uniform(-0.25, 0.25), 2)
+        y_per = round(random.uniform(-0.25, 0.25), 2)
+
+        counts = augment_image(dual_channel, aug_df, "translate", i, counts,
+                               leaf_path, mask_path, translate_img, x=x_per,
+                               y=y_per)
+
+    # P(zoom) = 0.35
+    if random.random() < 0.35:
+        # zoom in and out between 150% and 50%
+        x_per = round(random.uniform(1.5, 0.5), 2)
+        y_per = round(random.uniform(1.5, 0.5), 2)
+
+        counts = augment_image(dual_channel, aug_df, "zoom", i, counts,
+                               leaf_path, mask_path, zoom_in_out,
+                               x=x_per, y=y_per)
+
+    # P(crop) = 0.35
+    if random.random() < 0.35:
+        # crop between 5% and 30% of the image
+        v_per = round(random.uniform(0.05, 0.3), 2)
+        h_per = round(random.uniform(0.05, 0.3), 2)
+
+        counts = augment_image(dual_channel, aug_df, "crop", i, counts,
+                               leaf_path, mask_path, crop_img, v=v_per,
+                               h=h_per)
+
+    # P(rotate) = 0.35
+    if random.random() < 0.35:
+        # l element (-90;0) and r element (0;90) (degrees)
+        l_deg = round(random.random() * -90)
+        r_deg = round(random.random() * 90)
+
+        counts = augment_image(dual_channel, aug_df, "rotate", i, counts,
+                               leaf_path, mask_path, rotate_img, l=l_deg,
+                               r=r_deg)
+
+    # P(sheer) = 0.35
+    if random.random() < 0.35:
+        # l element (-30;0) and r element (0;30) (degrees)
+        l_deg = round(random.random() * -30)
+        r_deg = round(random.random() * 30)
+
+        counts = augment_image(dual_channel, aug_df, "shear", i, counts,
+                               leaf_path, mask_path, shear_img, l=l_deg,
+                               r=r_deg)
+
+    return aug_df, counts
+
+
+# *---------------------------- package __main__ -----------------------------*
+def augment_dataset(lseq: LeafSequence, mseq: MaskSequence):
+    """
+
+        :param lseq:
+        :param mseq:
+        :return:
+        """
+    # linked based on number:  <name>_<image_number>_<tile_number>
+    lseq.link_sequences(mseq)
+
+    # dataframe with the possible transformations as columns
+    aug_df = pd.DataFrame(index=range(len(lseq.image_objects)),
+                          columns=["leaf", "mask", "flip", "translate", "zoom",
+                                   "crop", "rotate", "shear"])
+
+    # setting random seed again to be sure
+    random.seed(3141)
+
+    # create augmented folders
+    base_path = Path(*list(Path(lseq.image_objects[0].path).parts)[:-3])
+    create_subfolders(base_path, "augmented")
+
+    # counts of augmented images accepted and rejected
+    counts = [0, 0]
+
+    with tqdm(total=len(lseq.image_objects), file=sys.stdout) as pbar:
+        for i, leaf in enumerate(lseq.image_objects):
+            # create the dual channel image, where leaf is channel 0 and mask
+            # is channel 1
+            mask = leaf.link
+
+            leaf_path = Path(leaf.path)
+            mask_path = Path(mask.path)
+
+            # checking links using numbers explicitly (requires
+            # <name>_<image_number>_<tile_number> naming format
+            assert (leaf_path.parts[-1].rsplit(".")[0].rsplit("_", 2)[-2:] ==
+                    mask_path.parts[-1].rsplit(".")[0].rsplit("_", 2)[-2:]), \
+                (f"leaf: {leaf_path} is incorrectly matched with mask:"
+                 f" {mask_path}; please check this")
+
+            aug_df["leaf"][i] = leaf_path
+            aug_df["mask"][i] = mask_path
+
+            leaf.load_image()
+            mask.load_image()
+
+            # Should load image array to keep loading consistent
+            dual_channel = stack_images(leaf.image_array, mask.image_array)
+
+            # save RAM
+            leaf.unload_extracted_images()
+            mask.unload_extracted_images()
+
+            aug_df, counts = augmentation_algorithm(dual_channel, aug_df, i,
+                                                    leaf_path, mask_path,
+                                                    counts)
+            pbar.update(1)
+
+    aug_df.to_csv(base_path.joinpath("augmented", "augmentation_details.csv"))
+
+    LOGGER.info(f"Added {counts[0]} images and rejected {counts[1]} images")
